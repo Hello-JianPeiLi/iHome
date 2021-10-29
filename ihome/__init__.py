8fee00966b1250a71535a5d9c50b57f16d25963c
Hello-JianPeiLi <root_pei@163.com>
2021-10-29 17:22:33 +0800

    完成日志功能

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -7,7 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_map
 from flask_wtf import CSRFProtect
 from flask_session import Session
+from logging.handlers import RotatingFileHandler
 import redis
+import logging
 
 # 数据库
 db = SQLAlchemy()
@@ -38,6 +40,17 @@ def create_app(config_name):
 
     # 为flask补充csrf防护
     CSRFProtect(app)
+
+    # 设置日志等级
+    logging.basicConfig(level=logging.DEBUG)  # 调式debug等级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler('logs/log', maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象(flask app使用的)添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
 
     # 注册蓝图
     from ihome import api_1_0
--- ihome/api_1_0/demo.py
+++ ihome/api_1_0/demo.py
@@ -3,8 +3,10 @@
 # @Time : 2021/10/29 14:41
 from . import api
 from ihome import db
+from flask import current_app
 
 
 @api.route('/index')
 def index():
+    current_app.logger.error('error msg')
     return 'index page'
