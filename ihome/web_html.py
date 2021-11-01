9c45f5b99a521bf83cad027461d0562543900e52
Hello-JianPeiLi <root_pei@163.com>
2021-11-01 16:25:58 +0800

    给cookies添加csrf_token

--- ihome/web_html.py
+++ ihome/web_html.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 # Author: JianPei
 # @Time : 2021/11/01 12:17
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, make_response
+from flask_wtf import csrf
 
 html = Blueprint('web_html', __name__)
 
@@ -15,5 +16,12 @@ def get_html(html_file_name):
     if html_file_name != 'favicon.ico':
         html_file_name = 'html/' + html_file_name
 
+    # 生成一个csrf_token
+    csrf_token = csrf.generate_csrf()
+
     # flask提供的返回静态文件的方法
-    return current_app.send_static_file(html_file_name)
+    rep = make_response(current_app.send_static_file(html_file_name))
+    # 设置cookies值，不设置有效期，每次重开就生成，保证安全
+    rep.set_cookie('csrf_token', csrf_token)
+
+    return rep
